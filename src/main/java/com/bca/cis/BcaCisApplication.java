01c6998df1d793f94d01dae6c42b90c391e458a5
benaleo <benaleo.bayu.satria@unictive.com>
2025-03-26 15:28:46 +0700

    hcp disable

--- src/main/java/com/bca/cis/BcaCisApplication.java
+++ src/main/java/com/bca/cis/BcaCisApplication.java
@@ -1,14 +1,12 @@
 package com.bca.cis;
 
-import bca.hcp.impl.HcpImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@Import(HcpImpl.class)
+//@Import(HcpImpl.class)
 public class BcaCisApplication {
 
     public static void main(String[] args) {
